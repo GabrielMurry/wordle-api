0f775bc924142b0d3c1f9aec9a636e73e48c62ab
Gabriel <gabrielmurryedh@gmail.com>
2022-09-19 22:17:33 -0700

    quick change in allowed Origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["https://wordle-clone-mern.onrender.com"];
 // "http://localhost:3000", for testing
 // "https://wordle-clone-mern.onrender.com" for deployment
 
