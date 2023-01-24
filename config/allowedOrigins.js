9008854872a5fecb0eb85989f0993f500d472f61
Gabriel <gabrielmurryedh@gmail.com>
2023-01-24 14:36:34 -0800

    testing deploy hook for server

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = ["https://wordle-mern.onrender.com"]; // allowing access for front-end
-// "http://localhost:3000", for testing
+// "http://localhost:3000", http://192.168.50.29:3000, for testing. Second is wifi IP address - can test on mobile
 // "https://wordle-mern.onrender.com" for deployment
 
 module.exports = allowedOrigins;
