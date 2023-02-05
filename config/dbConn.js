cdc9f725eb2ba19d6a465647288de1b5506fe177
Gabriel <gabrielmurryedh@gmail.com>
2023-02-05 11:45:06 -0800

    trying to turnh database uri env variable into string

--- config/dbConn.js
+++ config/dbConn.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URI, {
+    await mongoose.connect(`${process.env.DATABASE_URI}`, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
